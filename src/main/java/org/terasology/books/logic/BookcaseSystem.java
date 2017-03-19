31a8da8c0b3fd59add6e1535049916133a06b2e2
Cervator <cervator@gmail.com>
2017-03-19 16:57:01 -0400

    Fix #11 - bookcases only had their special logic on an authority client. Also adds javadoc and cleans up code a bit yay! Bumps patch version number.

--- src/main/java/org/terasology/books/logic/BookcaseSystem.java
+++ src/main/java/org/terasology/books/logic/BookcaseSystem.java
@@ -29,23 +29,36 @@ import org.terasology.world.block.entity.CreateBlockDropsEvent;
 
 import java.util.List;
 
-@RegisterSystem(RegisterMode.AUTHORITY)
+/**
+ * Simple system to allow bookcases to filter out non-books and drop contents when destroyed.
+ * This is a good simple example of a contrast with chests (that drop as a single item including its contents),
+ * and for showing a simple way to filter for specific objects.
+ *
+ * Setting the RegisterMode to ALWAYS lets the filtering trigger on a non-authority client ie. regular multiplayer clients.
+ */
+@RegisterSystem(RegisterMode.ALWAYS)
 public class BookcaseSystem extends BaseComponentSystem {
 
     /**
      * Check that only books can be put into a bookcase.
-     * @param event
-     * @param entity
+     * @param event the triggering event (somebody tried to put something into the bookcase)
+     * @param entity the object that is being submitted as an potential entry to the bookcase's inventory
      */
-    @ReceiveEvent
-    public void filterBook(BeforeItemPutInInventory event, EntityRef entity, BookcaseComponent bookcaseComponent) {
+    @ReceiveEvent(components = {BookcaseComponent.class})
+    public void filterBook(BeforeItemPutInInventory event, EntityRef entity) {
         if (!event.getItem().hasComponent(BookComponent.class)) {
             event.consume();
         }
     }
 
-    @ReceiveEvent
-    public void onDestroyBookCase(CreateBlockDropsEvent event, EntityRef entity, BookcaseComponent bookcaseComponent) {
+    /**
+     * On destruction of a bookcase go through its inventory and drop all items on the ground.
+     * This
+     * @param event the triggering event (something caused block drops - the bookcase was destroyed)
+     * @param entity the bookcase's entity, has the target inventory to go through
+     */
+    @ReceiveEvent(components = {BookcaseComponent.class})
+    public void onDestroyBookCase(CreateBlockDropsEvent event, EntityRef entity) {
         if (entity.hasComponent(InventoryComponent.class)) {
             Vector3f location = entity.getComponent(LocationComponent.class).getWorldPosition();
             InventoryComponent inventory = entity.getComponent(InventoryComponent.class);
